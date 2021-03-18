75b8ef55d38aa09b744dec4edb09a350759af5e5
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-18 20:43:26 +0700

    Update stickers_v2.py

--- userbot/modules/stickers_v2.py
+++ userbot/modules/stickers_v2.py
@@ -64,7 +64,7 @@ async def _(event):
             return
             await event.edit("Sorry i cant't convert it check wheter is non animated sticker or not")
         else:
-            await event.client.send_messages(conv.chat_id, r3.id, reply_to=reply_message.id)
+            await bot.send_messages(event.chat_id, r3.id, reply_to=reply_message.id)
             await event.client.delete_messages(
                     conv.chat_id, [msg.id, r1.id, r2.id, r3.id])
             await event.delete()
