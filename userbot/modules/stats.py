8e4e8af9ae96b1d304704dc667c98c00bbaf024f
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-18 17:12:39 +0700

    Update stats.py

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -1,52 +1,50 @@
-import random
-import requests
-from asyncio.exceptions import TimeoutError
-
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from userbot import CMD_HELP, bot
+import io
+from userbot import bot, CMD_HELP
 from userbot.events import register
 
 @register(outgoing=True, pattern=r"^\.ustat")
-async def quotess(qotli):
-    if qotli.fwd_from:
+async def _(event):
+    if event.fwd_from:
         return
-    if not qotli.reply_to_msg_id:
-        return await qotli.edit("```Balas di Pesan Goblok!!.```")
-    reply_message = await qotli.get_reply_message()
+    if not event.reply_to_msg_id:
+        return
+    await event.edit("Balas di Pesan Goblok!!")
+    reply_message = await event.get_reply_message()
     if not reply_message.text:
-        return await qotli.edit("```Balas di Pesan Goblok!!```")
+        return
+    await event.edit("Balas di Pesan Goblok!!")
     chat = "@tgscanrobot"
-    if reply_message.sender.bot:
-        return await qotli.edit("```Balas di Pesan Goblok!!.```")
-    await qotli.edit("```Checking......```")
-    try:
-        async with bot.conversation(chat) as conv:
-            try:
+    await event.edit("Checking....")
+    async with event.client.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=1557162396))
+            msg = await event.client.forward_messages(chat, reply_message)
+            response = await response
+        except YouBlockedUserError:
+            await event.reply("unblock me @tgscanrobot to work")
+            return
+        if response.text.startswith("I understand only stickers"):
+            await event.edit("Sorry i cant't convert it check wheter is non animated sticker or not")
+        else:
+            response = conv.wait_event(
+                events.NewMessage(
+                    incoming=True,
+                    from_users=1557162396))
+            response = await response
+            if response.text.startswith("Information"):
                 response = conv.wait_event(
                     events.NewMessage(
                         incoming=True,
                         from_users=1557162396))
-                response2 = conv.wait_event(
-                    events.NewMessage(
-                        incoming=True,
-                        from_users=1557162396))
-                msg = await bot.forward_messages(chat, reply_message)
                 response = await response
-                response2 = await response
-                """ - don't spam notif - """
-                await bot.send_read_acknowledge(conv.chat_id)
-            except YouBlockedUserError:
-                return await qotli.reply("```Please unblock @tgscanrobot and try again```")
-            if response.text.startswith("Hi!"):
-                await qotli.edit("```Can you kindly disable your forward privacy settings for good?```")
+                await event.delete()
+                await event.client.send_message(event.chat_id, response.message, reply_to=reply_message.id)
+                await event.client.delete_message(event.chat_id, [msg.id, response.id])
             else:
-                await qotli.delete()
-                await bot.send_messages(qotli.chat_id, response.message)
-                await bot.send_messages(qotli.chat_id, response2.message)
-                await bot.send_read_acknowledge(qotli.chat_id)
-                """ - cleanup chat after completed - """
-                await qotli.client.delete_messages(conv.chat_id,
-                                                   [msg.id, response.id])
-    except TimeoutError:
-        await qotli.edit()
+                await event.edit("try again")
+        await bot.send_read_acknowledge(conv.chat_id)
