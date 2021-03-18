9bcce6c178ae6d5c2c774f20f22fcd0a55525406
ImFine <79558474+ximfine@users.noreply.github.com>
2021-03-18 17:24:47 +0700

    Update stats.py

--- userbot/modules/stats.py
+++ userbot/modules/stats.py
@@ -13,7 +13,6 @@ async def _(event):
     reply_message = await event.get_reply_message()
     if not reply_message.text:
         return await event.edit("```Balas di Pesan Goblok!!```")
-    await event.edit("Balas di Pesan Goblok!!")
     chat = "@tgscanrobot"
     await event.edit("Checking....")
     async with event.client.conversation(chat) as conv:
@@ -38,7 +37,8 @@ async def _(event):
                 response = await response
                 await event.delete()
                 await event.client.send_message(event.chat_id, response.message, reply_to=reply_message.id)
-                await event.client.delete_message(event.chat_id, [msg.id, response.id])
+                await event.client.delete_messages(conv.chat_id,
+                                                   [msg.id, response.id])
             else:
                 await event.edit("try again")
         await bot.send_read_acknowledge(conv.chat_id)
